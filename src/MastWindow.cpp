20130851d490b56345ef65ca04d8a0b2bbbbee89
Aerodos12 <weldon.henson97@gmail.com>
2021-04-15 12:50:12 -0400

    New functionality (for use with MastWarden)

--- src/MastWindow.cpp
+++ src/MastWindow.cpp
@@ -2,7 +2,7 @@
 #include <curses.h>
 #include "MastWindow.h"
 
-Mast::TK::Window::Window(int height, int width, int startx, int starty){
+Mast::TK::Window::Window(int height, int width, int startx, int starty, WindowType type, Window* root_win){
   border = (Mast::TK::WindowBorder){
     .height = height,
     .width = width,
@@ -17,7 +17,14 @@ Mast::TK::Window::Window(int height, int width, int startx, int starty){
     .br = '+',
     .tr = '+'
   };
-
+  switch(type){
+      case Mast::TK::WindowType::RegularWindow:
+          window_pvt = newwin(border.height, border.width, border.starty, border.startx);
+          break;
+      case Mast::TK::WindowType::SubWindow:
+          window_pvt = derwin(root_win->getWindow(), border.height, border.width, border.starty, border.startx);
+          break;
+  }
 }
 
 void Mast::TK::Window::toggle_keypad(bool on){
@@ -36,9 +43,23 @@ void Mast::TK::Window::set_menu(MENU*& menu) {
   set_menu_win(menu, this->window_pvt);
 }
 
+void Mast::TK::Window::set_menu_subw(MENU*& menu) {
+  set_menu_sub(menu, this->window_pvt);
+}
+
+int Mast::TK::Window::get_input(){
+    return wgetch(this->window_pvt);
+}
+
+WINDOW* Mast::TK::Window::getWindow() const {
+    return this->window_pvt;
+}
+
+void Mast::TK::Window::refresh() {
+    wrefresh(this->window_pvt);
+}
 
 void Mast::TK::Window::draw(bool use_border) {
-  window_pvt = newwin(border.height, border.width, border.starty, border.startx);
   int x, y, w, h;
   x = border.startx;
   y = border.starty; 
--- src/MastWindow.h
+++ src/MastWindow.h
@@ -20,15 +20,25 @@ namespace Mast {
       char br;
       char tr;
     } WindowBorder;
+    
+    enum class WindowType {
+      SubWindow,
+      RegularWindow
+    };
+    
     class Window {
     public:
       WindowBorder border;
       void draw(bool use_border = true);
       void toggle_keypad(bool on);
-      Window(int height, int width, int startx, int starty);
+      Window(int height, int width, int startx, int starty, WindowType type, Window* root_win = nullptr);
       ~Window();
       void set_border(WindowBorder border2);
       void set_menu(MENU*& menu);
+      void set_menu_subw(MENU*& menu);
+      int get_input();
+      WINDOW* getWindow() const;
+      void refresh();
     private:
       WINDOW* window_pvt;
       
